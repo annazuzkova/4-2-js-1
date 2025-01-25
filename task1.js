d76745fbdf06d3a801de1e467d9b4f6669ec696c
AnnaZuzkova <famzu2018@gmail.com>
2025-01-25 18:20:48 +0200

    done

--- task1.js
+++ task1.js
@@ -1,8 +1,11 @@
 const body = document.querySelector(".body");
 const input = document.querySelector(".slider__input");
 input.addEventListener(
-  "scroll",
+  "change",
   _.debounce(() => {
-    body.style.backgroundColor = "red";
+    const red = Math.random() * (250 - 1) + 1;
+    const green = Math.random() * (250 - 1) + 1;
+    const blue = Math.random() * (250 - 1) + 1;
+    body.style.backgroundColor = `rgb(${red},${green},${blue})`;
   }, 3000)
 );
--- task2.js
+++ task2.js
@@ -1,7 +1,9 @@
 const box = document.querySelector("#box");
+let width = 100;
 box.addEventListener(
   "mousemove",
   _.debounce(() => {
-    box.style.width = "100px";
+    box.style.width = `${width}px`;
+    width += 100;
   }, 1000)
 );
